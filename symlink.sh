a3408adba66dc9ade4f68f5ddb46c4544936aec4
Dmytro Piliugin <dmytro.piliugin@gmail.com>
2018-07-15 23:11:55 +0300

    symlink.sh for linking files in dir to ~

---
+++ symlink.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+basepath=$(cd $(dirname $0);pwd)
+
+# symlink dotfiles into ~
+files=.*
+for file in $files
+do
+    if [ $file != "." -a $file != ".." -a $file != ".git" -a $file != ".gitignore" ] ; then
+        ln -sf $basepath/$file ~
+    fi
+done
+
+ln -sf $basepath/vim ~
+ln -sf $basepath/gitignore ~/.gitignore
